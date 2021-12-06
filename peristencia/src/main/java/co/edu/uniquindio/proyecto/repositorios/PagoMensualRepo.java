d806264d30dfab2d659489f30ef02eec54be63d4
davidcorredor13 <64997596+davidcorredor13@users.noreply.github.com>
2021-12-06 15:23:26 -0500

    Clase pago mensual

--- peristencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/PagoMensualRepo.java
+++ peristencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/PagoMensualRepo.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.proyecto.repositorios;
 
 
+
 import co.edu.uniquindio.proyecto.entidades.Mascota;
 import co.edu.uniquindio.proyecto.entidades.PagoMensual;
 import org.springframework.data.jpa.repository.JpaRepository;
